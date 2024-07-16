74a3ccb1b68d1ff566a8b7c7f05559ca1c6e9cf0
Paijit SE <113278749+PaijitSE@users.noreply.github.com>
2024-07-16 20:22:00 +0700

    Update

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+require_once('script/Myscript.php');
+$db_handle = new myDBControl();
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +18,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
-<body>
+<body class="body">
     <!-- <h6>ส่วนที่ 1 : ธนากร</h6> -->
     <div class="section1">
         <div class="main">
@@ -30,26 +35,30 @@
             <li><a href="#">เข้าสู่ระบบ</a></li>
         </ul>
     </div>
-    <div class="container">
-        <!-- <h6>ส่วนที่ 2 : กันติศา & อธิศ</h6> -->
+    <!-- <h6>ส่วนที่ 2 : กันติศา & อธิศ</h6> -->
+    <div class="section0">
         <div id="demo" class="carousel slide section2" data-bs-ride="carousel">
             <!-- Indicators/dots -->
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
                 <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
                 <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
             </div>
 
             <!-- The slideshow/carousel -->
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="img/l1.jfif" class="rounded-4" alt="" class="d-block" style="width:100%">
+                    <img src="img/banner1.png" class="rounded-4" alt="" class="d-block" style="width:100%">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/l2.jfif" class="rounded-4" alt="" class="d-block" style="width:100%">
+                    <img src="img/banner2.png" class="rounded-4" alt="" class="d-block" style="width:100%">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/l3.jfif" class="rounded-4" alt="" class="d-block" style="width:100%">
+                    <img src="img/banner3.png" class="rounded-4" alt="" class="d-block" style="width:100%">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/banner4.png" class="rounded-4" alt="" class="d-block" style="width:100%">
                 </div>
             </div>
 
@@ -64,34 +73,61 @@
         <!-- <h6>ส่วนที่ 3 : คุณาวุฒิ</h6> -->
         <div class="section3">
             <h6>สินค้าขายดี / สินค้าแนะนำ</h6>
-            <table>
-                <tr>
-                    <?php for ($x = 1; $x <= 5; $x++) {
-                    ?>
-                        <td>
-                            <img src="img/image2.jpg">
-                            <p>100 บาท</p>
-                        </td>
-                    <?php } ?>
-                </tr>
-            </table>
-        </div>
-        <h5 style="margin-top:10px; font-weight:bold;">ส่วนที่ 4 : สุทธิดา ********* ยังขาด</h5>
-        <!-- <h6>ส่วนที่ 5 : พุฒินาท</h6> -->
-        <div class="secction5">
-            <div class="product">
+            <div class="productRecommend">
+
+                <!-- ค้นหาข้อมูล และรับข้อมูล -->
                 <?php
-                for ($x = 0; $x <= 30; $x++) {
-                    if ($x == 3) continue;
+                $data = $db_handle->Textquery("SELECT * FROM PRODUCT_RECOMMEND");
+                ?>
+
+                <!-- แสดงข้อมูล -->
+                <?php for ($x = 0; $x <= 4; $x++) {
+                ?>
+                    <div class="productBox">
+                        <img src="<?php echo $data[$x]['Product_picture']; ?>">
+                        <p><?php echo number_format($data[$x]['Product_price']); ?> บาท</p>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+        <!-- ส่วนที่ 4 : สุทธิดา </h5> -->
+        <div class="section4">
+            <div class="SLeft">
+                <input type="text" placeholder="ระบุคำค้น" name="keyword">
+                <button name="act" onclick="window.location='index.php?search=ส่งคำค้นไปจ้า'">Search</button>
+            </div>
+            <div class="SRight">
+                <?php $Typedetail = $db_handle->Textquery("SELECT * FROM PRODUCT_TYPE;") ?>
+                <select name="Stype" onchange="window.location='index.php?search=ส่งประเภทไปจ้า'">
+                    <?php if (empty($Typedetail)) { ?>
+                        <option selected>ไม่มีประเภทสินค้า</option>
+                    <?php } else { ?>
+                        <option selected>เลือกประเภทสินค้า</option>
+                        <?php foreach ($Typedetail as $key => $value) { ?>
+                            <option value="<?php echo $Typedetail[$key]["Type_no"]; ?>">
+                                <?php echo $Typedetail[$key]["Type_name"]; ?></option>
+                    <?php }
+                    } ?>
+                </select>
+            </div>
+        </div>
+        <!-- <h6>ส่วนที่ 5 : พุฒินาท</h6> -->
+        <div class="section5">
+            <div class="product">
+
+                <?php
+                $data = $db_handle->Textquery("SELECT * FROM ALLPRODUCT;");
+                ?>
+                <?php foreach ($data as $key => $value) {
                 ?>
                     <div class="productBox boxSize_M">
-                        <img class="productImg" src="img/7.png">
+                        <img class="productImg" src="<?php echo $data[$key]['Product_picture']; ?>">
                         <div class=" productTxt">
-                            <h6><b>รหัสสินค้า </b>S0x</b></h6>
-                            <p><b>Type : </b>สำเร็จรูป</b></p>
-                            <p><b>Name : </b>ชื่อสินค้า</b></p>
-                            <p><b>Price : </b>199</b> บาท/ <b>Stock : </b>100</b> ea.</p>
-                            <button>เลือกซื้อ</button>
+                            <h6><b>รหัสสินค้า </b><?php echo $data[$key]['Product_id']; ?></b></h6>
+                            <p><b>Type : </b><?php echo $data[$key]['New_type']; ?> </b></p>
+                            <p><b>Name : </b><?php echo $data[$key]['New_name']; ?>....</b></p>
+                            <p><b>Price : </b><?php echo number_format($data[$key]['Product_price'], 2); ?></p>
+                            <button onclick="alert('ฉันเลือกซื้อสินค้ารายการนี้....');">เลือกซื้อ</button>
                         </div>
                     </div>
                 <?php } ?>
